5bd7db562991f2d1941c6a26a9603311d50e1b60
Gareth Dawson <gareth-dawson@hotmail.co.uk>
2021-08-18 20:24:06 +0100

    added balance

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+# TODO ALL OWNER COMMANDS -> check for user id in bot_owner()
+# TODO ALL STAFF COMMANDS -> check for user id in bot_staff()
+
 from discord.ext import commands
 from dotenv import load_dotenv
 from datetime import timedelta
@@ -17,14 +20,6 @@ filepath_users = 'users\\users.json'
 start_time = time.time()
 load_dotenv()
 
-def pre_alpha_testers():
-	pre_alpha_ids = [389897179701182465]
-	return (pre_alpha_ids)
-
-def bot_staff():
-	bot_staff_ids = [389897179701182465]
-	return (bot_staff_ids)
-
 @client.event
 async def on_ready():
 	print(f'===================================================')
@@ -33,16 +28,50 @@ async def on_ready():
 	print(f'===================================================')
 
 ##########################################################################################################
+###                                              FUNCTIONS                                             ###
+##########################################################################################################
+
+def bot_owner():
+	bot_owner_ids = [389897179701182465]
+	return (bot_owner_ids)
+
+def bot_staff():
+	bot_staff_ids = [389897179701182465]
+	return (bot_staff_ids)
+
+def card_algorithm():
+	random_number = numpy.random.randint(low=0, high=100, size=1)
+	if random_number <= 2:
+		result = 'legendary'
+	elif 2 < random_number <= 8:
+		result = 'rare'
+	elif 8 < random_number <= 25:
+		result = 'uncommon'
+	else:
+		result = 'common'
+	return(result)
+
+def balance_algorithm():
+	random_number = numpy.random.randint(low=0, high=100, size=1)
+	if random_number <= 2:
+		result = 650
+	elif 2 < random_number <= 8:
+		result = 250
+	elif 8 < random_number <= 25:
+		result = 80
+	else:
+		result = 20
+	return(result)
+
+##########################################################################################################
 ###                                           OWNER-COMMANDS                                           ###
 ##########################################################################################################
 
 @client.group(invoke_without_command=True)
-@commands.is_owner()
 async def award(ctx):
 	pass
 
 @award.group()
-@commands.is_owner()
 async def balance(ctx, other: discord.Member, amount):
 	sent_user = str(other.id)
 	sent_amount = int(amount)
@@ -51,20 +80,12 @@ async def balance(ctx, other: discord.Member, amount):
 	users[sent_user]['currency'] += sent_amount
 	with open(filepath_users, 'w') as f:
 		json.dump(users,f, indent=4)
-	
-	await ctx.send(f'{sent_amount} has been gifted to {other.mention}')
-
-@award.group()
-@commands.is_owner()
-async def card(ctx, other: discord.Member, card):
-	pass
+	sent_formatted = ('{:,}').format(sent_amount)
+	await ctx.send(f'`{other} has been awarded with ${sent_formatted}`')
 
 ##########################################################################################################
 ###                                           STAFF-COMMANDS                                           ###
 ##########################################################################################################
-
-# TODO bot -> check for user id in bot_staff()
-# TODO status <group> -> check for user id in bot_staff()
 
 @client.command()
 async def bot(ctx):
@@ -134,71 +155,20 @@ async def balance(ctx):
 	string_user_id = str(ctx.author.id)
 	with open(filepath_users,'r') as f:
 		users = json.load(f)
-	balance = users[string_user_id]['currency']
-	formatted = ('{:,}').format(balance)
-	with open(filepath_users, 'w') as f:
-		json.dump(users,f, indent=4)
-	embed = discord.Embed(description=f'Your current balance is ${formatted}', colour=discord.Color.from_rgb(0,255,133))
-	embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
-	await ctx.send(embed=embed)
-
-##########################################################################################################
-###                                           TESTER-COMMANDS                                          ###
-##########################################################################################################
-
-# TODO check_card_algorithm -> check for user id in pre_alpha_testers()
-# TODO check_balance_algorithm -> check for user id in pre_alpha_testers()
-
-@client.command()
-async def check_card_algorithm(ctx):
-	result = card_algorithm()
-	embed = discord.Embed(description=f'{result}', colour=discord.Color.from_rgb(0,0,0))
-	embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
-	await ctx.send(embed=embed)
-
-@client.command()
-async def check_balance_algorithm(ctx):
-	result = balance_algorithm()
-	embed = discord.Embed(description=f'{result}', colour=discord.Color.from_rgb(0,0,0))
-	embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
-	await ctx.send(embed=embed)
-
-###########################################################################################################
-###                                          ERROR-HANDLING                                             ###
-###########################################################################################################
-
-@client.event
-async def on_command_error(ctx: commands.Context, error: commands.CommandError):
-	if isinstance(error, commands.NotOwner):
-		embed = discord.Embed(description='Oops, this command is only supposed to be used by owner.', colour=discord.Color.from_rgb(225,29,98))
+	if string_user_id in users:
+		balance = users[string_user_id]['currency']
+		formatted = ('{:,}').format(balance)
+		with open(filepath_users, 'w') as f:
+			json.dump(users,f, indent=4)
+		embed = discord.Embed(description=f'Your current balance is ${formatted}', colour=discord.Color.from_rgb(0,255,133))
+		embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
+		await ctx.send(embed=embed)
+	else:
+		embed = discord.Embed(description=f'Oops, it looks like you don\'t have an account.', colour=discord.Color.from_rgb(225,29,98))
+		embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
+		embed.set_footer(text='You can use !register to create an account.')
 		await ctx.send(embed=embed)
 
 ###########################################################################################################
-###                                            ALGORITHMS                                               ###
-###########################################################################################################
-
-def card_algorithm():
-	random_number = numpy.random.randint(low=0, high=100, size=1)
-	if random_number <= 2:
-		result = 'legendary'
-	elif 2 < random_number <= 8:
-		result = 'rare'
-	elif 8 < random_number <= 25:
-		result = 'uncommon'
-	else:
-		result = 'common'
-	return(result)
-
-def balance_algorithm():
-	random_number = numpy.random.randint(low=0, high=100, size=1)
-	if random_number <= 2:
-		result = 650
-	elif 2 < random_number <= 8:
-		result = 250
-	elif 8 < random_number <= 25:
-		result = 80
-	else:
-		result = 20
-	return(result)
 
 client.run(os.getenv('TOKEN_MAIN'))
