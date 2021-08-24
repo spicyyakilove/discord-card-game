bebe6dfd2af5d28054aefb048817e1b273b83555
Gareth Dawson <gwdawson.work@gmail.com>
2021-08-24 19:21:49 +0100

    added card image to daily embed

--- commands/daily.py
+++ commands/daily.py
@@ -22,11 +22,10 @@ class daily(commands.Cog):
         g_space = ' ' * (31 - len(game))
         t_space = ' ' * (31 - len(team))
         p_space = ' ' * (31 - len(player))
-        r_space = ' ' * (31 - len(rarity))
-        i_space = ' ' * (30 - len(issue_str))
-        c_space=  ' ' * (31 - len(code))
-        embed = discord.Embed(description=f'`{game}{g_space}`\n`{team}{t_space}`\n`{player}{p_space}`\n`{rarity}{r_space}`\n`#{issue+1}{i_space}`\n`{code}{c_space}`', colour=discord.Color.from_rgb(0,255,133))
+        c_space = ' ' * (30 - (len(code) + len(issue_str)))
+        embed = discord.Embed(description=f'`{game}{g_space}`\n`{team}{t_space}`\n`{player}{p_space}`\n`{code}#{issue+1}{c_space}`', colour=discord.Color.from_rgb(0,0,0))
         embed.set_author(name='DAILY', icon_url=f'{ctx.author.avatar_url}')
+        embed.set_image(url=f'{image}')
         await ctx.send(embed=embed)
 
 def setup(client):
