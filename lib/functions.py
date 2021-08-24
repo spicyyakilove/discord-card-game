fef2390d8c26e36cfb5244228abfa05bd7b00cf4
Gareth Dawson <gwdawson.work@gmail.com>
2021-08-24 18:33:47 +0100

    changed variables to lowercase

--- lib/functions.py
+++ lib/functions.py
@@ -6,17 +6,17 @@ cursor = database.cursor()
 
 def select_card_rarity():
     random_number = random.randint(0, 1000)
-    if random_number <= 1: #0.1%
+    if random_number <= 1:
         result = '6'
-    elif 1 < random_number <= 51: #5%
+    elif 1 < random_number <= 51:
         result = '5'
-    elif 51 < random_number <= 151: #10% 
+    elif 51 < random_number <= 151:
         result = '4'
-    elif 151 < random_number <= 301: #15%
+    elif 151 < random_number <= 301:
         result = '3'
-    elif 301 < random_number <= 501: #20%
+    elif 301 < random_number <= 501:
         result = '2'
-    elif 501 < random_number <= 1000: #49.9%
+    elif 501 < random_number <= 1000:
         result = '1'
     else: result = 'HOW DID WE GET HERE?'
     return(result)
@@ -25,12 +25,12 @@ def select_random_card(rarity):
     query1 = f"SELECT code FROM Cards WHERE rarity={rarity};"
     cursor.execute(query1); database.commit();
     cardlist = cursor.fetchall()
-    CODE = random.choice(cardlist)
-    CODE = CODE[0]
-    query2 = f"SELECT game, team, player, issue, image FROM Cards WHERE code='{CODE}';"
+    code = random.choice(cardlist)
+    code = code[0]
+    query2 = f"SELECT game, team, player, issue, image FROM Cards WHERE code='{code}';"
     cursor.execute(query2); database.commit();
-    GAME, TEAM, PLAYER, ISSUE, IMAGE = cursor.fetchall()[0]
-    return(CODE, GAME, TEAM, PLAYER, ISSUE, IMAGE)
+    game, team, player, issue, image = cursor.fetchall()[0]
+    return(code, game, team, player, issue, image)
 
 def insert_card(code, game, team, player, rarity, issue, userid):
     query1 = f"INSERT INTO UserCards VALUES ('{code}', '{game}', '{team}', '{player}', {rarity}, {issue+1}, {userid})"
@@ -40,17 +40,17 @@ def insert_card(code, game, team, player, rarity, issue, userid):
 
 def select_balance_rarity():
     random_number = random.randint(0, 1000)
-    if random_number <= 1: #0.1%
+    if random_number <= 1:
         result = '600'
-    elif 1 < random_number <= 51: #5%
+    elif 1 < random_number <= 51:
         result = '500'
-    elif 51 < random_number <= 151: #10% 
+    elif 51 < random_number <= 151:
         result = '400'
-    elif 151 < random_number <= 301: #15%
+    elif 151 < random_number <= 301:
         result = '300'
-    elif 301 < random_number <= 501: #20%
+    elif 301 < random_number <= 501:
         result = '200'
-    elif 501 < random_number <= 1000: #49.9%
+    elif 501 < random_number <= 1000:
         result = '100'
     else: result = 'HOW DID WE GET HERE?'
     return(result)
