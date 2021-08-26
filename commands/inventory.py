0fa36efcfb2ed07adfb126aa79715fe93540fa64
Gareth Dawson <gwdawson.work@gmail.com>
2021-08-26 21:14:02 +0100

    commit so snow can help my stupidity

--- commands/inventory.py
+++ commands/inventory.py
@@ -2,6 +2,7 @@ import discord
 import random
 import sqlite3
 
+from disputils import BotEmbedPaginator
 from discord.ext import commands
 from lib.functions import *
 
@@ -20,23 +21,50 @@ class inventory(commands.Cog):
         else:
             username = str(user.name)
             userid = int(user.id)
+        
         cursor.execute(f'SELECT code, game, team, player, teamyear, rarity, issue FROM UserCards WHERE userid={userid};')
-        allcardlist = cursor.fetchall()
-        cursor.execute(f'SELECT code, game, team, player, teamyear, rarity, issue FROM UserCards WHERE userid={userid} ORDER BY rarity DESC, team ASC, player ASC, issue ASC LIMIT 20;')
         cardlist = cursor.fetchall()
-        desc_title = f'{username} has **{len(allcardlist)}** cards in their inventory!'
+        embeds = []
+
+        desc_title = f'{username} has **{len(cardlist)}** cards in their inventory!'
         if len(cardlist) == 0:
             embed = discord.Embed(description='You don\'t seem to have any cards yet!', colour=discord.Color.from_rgb(255,0,0))
             embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
             embed.set_footer(text='You can use !daily to claim your first card.')
             await ctx.send(embed=embed)
+        
         else:
-            desc = ''
-            for i in cardlist:
-                desc = desc + f'\n` {i[5]}⭐ `—` {i[2]} `—` {i[3]} `—` {i[0]}#{i[6]} `'
-            embed = discord.Embed(description=f'{desc_title}\n{desc}', colour=discord.Color.from_rgb(0,0,0))
-            embed.set_author(name=f'INVENTORY', icon_url=f'{ctx.author.avatar_url}')
-            await ctx.send(embed=embed)
+            ####################################################################################################
+            ## SNOW ⬇️⬇️                                                                                     ##
+            ####################################################################################################
+            totalpages = (len(cardlist) // 20)
+            print(totalpages)
+            remainder = len(cardlist) % 20
+            i = 0
+            for i in range(totalpages):
+                desc = ''
+                for k in range(20):
+                    j = cardlist[(i*20)+k]
+                    desc = desc + f'\n` {j[5]}⭐ `—` {j[2]} {j[3]} `—` {j[0]}#{j[6]} `'
+                embeds.append(discord.Embed(description=f'{desc_title}\n{desc}', colour=discord.Color.from_rgb(0,0,0))
+                .set_author(name='INVENTORY', icon_url=f'{ctx.author.avatar_url}'))
+            for i in range(remainder):
+                desc = desc + f'\n` {cardlist[totalpages+i][5]}⭐ `—` {cardlist[totalpages+i][2]} {cardlist[totalpages+i][3]} `—` {cardlist[totalpages+i][0]}#{cardlist[totalpages+i][6]} `'
+                embeds.append(discord.Embed(description=f'{desc_title}\n{desc}', colour=discord.Color.from_rgb(0,0,0))
+                .set_author(name='INVENTORY', icon_url=f'{ctx.author.avatar_url}'))
+            paginator = BotEmbedPaginator(ctx, embeds)
+            await paginator.run()
+            ####################################################################################################
+            ## SNOW ⬆️⬆️                                                                                     ##
+            ####################################################################################################
+
+            # for i in cardlist:
+            #     desc = ''
+            #     desc = desc + f'\n` {i[5]}⭐ `—` {i[2]} {i[3]} `—` {i[0]}#{i[6]} `'
+            #     embeds.append(discord.Embed(description=f'{desc_title}\n{desc}', colour=discord.Color.from_rgb(0,0,0))
+            #     .set_author(name='INVENTORY', icon_url=f'{ctx.author.avatar_url}'))
+            # paginator = BotEmbedPaginator(ctx, embeds)
+            # await paginator.run()
 
 def setup(client):
     client.add_cog(inventory(client))
