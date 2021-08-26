a2126977c82fb8f96e3693568b91fdb4f7357c70
Gareth Dawson <gwdawson.work@gmail.com>
2021-08-26 15:52:32 +0100

    updated inventory

--- commands/inventory.py
+++ commands/inventory.py
@@ -13,20 +13,28 @@ class inventory(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def inventory(self, ctx):
-        userid = int(ctx.author.id)
-        cursor.execute(f'SELECT code, game, team, player, teamyear, rarity, issue FROM UserCards WHERE userid={userid} ORDER BY rarity ASC, team ASC, player ASC, issue ASC LIMIT 20;')
+    async def inventory(self, ctx, user:discord.Member=None):
+        if user == None:
+            username = str(ctx.author.name)
+            userid = int(ctx.author.id)
+        else:
+            username = str(user.name)
+            userid = int(user.id)
+        cursor.execute(f'SELECT code, game, team, player, teamyear, rarity, issue FROM UserCards WHERE userid={userid};')
+        allcardlist = cursor.fetchall()
+        cursor.execute(f'SELECT code, game, team, player, teamyear, rarity, issue FROM UserCards WHERE userid={userid} ORDER BY rarity DESC, team ASC, player ASC, issue ASC LIMIT 20;')
         cardlist = cursor.fetchall()
+        desc_title = f'{username} has **{len(allcardlist)}** cards in their inventory!'
         if len(cardlist) == 0:
             embed = discord.Embed(description='You don\'t seem to have any cards yet!', colour=discord.Color.from_rgb(255,0,0))
             embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
-            embed.set_footer(text='You can use !help to see a full list of commands.')
+            embed.set_footer(text='You can use !daily to claim your first card.')
             await ctx.send(embed=embed)
         else:
             desc = ''
             for i in cardlist:
                 desc = desc + f'\n` {i[5]}⭐ `—` {i[2]} `—` {i[3]} `—` {i[0]}#{i[6]} `'
-            embed = discord.Embed(description=f'{desc}', colour=discord.Color.from_rgb(0,0,0))
+            embed = discord.Embed(description=f'{desc_title}\n{desc}', colour=discord.Color.from_rgb(0,0,0))
             embed.set_author(name=f'INVENTORY', icon_url=f'{ctx.author.avatar_url}')
             await ctx.send(embed=embed)
 
