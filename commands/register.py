6c6d4e988a13888e5d731d02614b7bf5cde91df4
Gareth Dawson <gwdawson.work@gmail.com>
2021-08-26 13:25:35 +0100

    added error for users that already are already registered

--- commands/register.py
+++ commands/register.py
@@ -16,15 +16,22 @@ class register(commands.Cog):
     @commands.command()
     async def register(self, ctx):
         userid = int(ctx.author.id)
-        blacklist = 0
-        currency = 500
-        date = datetime.datetime.now().date()
-        query = f"INSERT INTO Users VALUES ({int(userid)}, {int(blacklist)}, {int(currency)}, '{str(date)}');"
-        cursor.execute(query); database.commit()
-        embed = discord.Embed(description='Congratulations, your account has been successfully created.\n', colour=discord.Color.from_rgb(0,0,0))
-        embed.set_author(name='REGISTER', icon_url=f'{ctx.author.avatar_url}')
-        embed.set_footer(text='You can use !daily to claim your first card.')
-        await ctx.send(embed=embed)
+        registered = check_users_exists(userid)
+        if registered != None:
+            embed = discord.Embed(description='Oops, it looks like you already have an account.', colour=discord.Color.from_rgb(255,0,0))
+            embed.set_author(name=f'{ctx.author.name}', icon_url=f'{ctx.author.avatar_url}')
+            embed.set_footer(text='You can use !help to see a full list of commands.')
+            await ctx.send(embed=embed)
+        else:
+            blacklist = 0
+            currency = 500
+            date = datetime.datetime.now().date()
+            query = f"INSERT INTO Users VALUES ({int(userid)}, {int(blacklist)}, {int(currency)}, '{str(date)}');"
+            cursor.execute(query); database.commit()
+            embed = discord.Embed(description='Congratulations, your account has been successfully created.\n', colour=discord.Color.from_rgb(0,0,0))
+            embed.set_author(name='REGISTER', icon_url=f'{ctx.author.avatar_url}')
+            embed.set_footer(text='You can use !daily to claim your first card.')
+            await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(register(client))
