fafe4245793fb457112beaad887067d9ccef3165
Gareth Dawson <gwdawson.work@gmail.com>
2021-08-27 17:47:33 +0100

    added bot status commmand

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import discord
 import dotenv
 import sqlite3
+import datetime
+import time
 import os
 
+from datetime import timedelta
 from discord.ext import commands
 from dotenv import load_dotenv
 
 client = commands.Bot(command_prefix='!', case_insensitive=True, help_command=None)
 database = sqlite3.connect('./database/database.sqlite')
 cursor = database.cursor()
+start_time = time.time()
 load_dotenv()
 
 for filename in os.listdir('./commands'):
     if filename.endswith('.py'):
         client.load_extension(f'commands.{filename[:-3]}')
+
+@client.command()
+async def bot(ctx):
+    global startTime
+    uptime = str(datetime.timedelta(seconds=int(round(time.time()-start_time))))
+    await ctx.send(f'`Status: Online | Ping: {round(client.latency * 1000)}ms | Uptime: {uptime}`')
 
 @client.command()
 @commands.is_owner()
